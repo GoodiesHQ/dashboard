98a5d2d78a050d56f789b67e98e2e871bee2317d
GoodiesHQ <aarcher73k@gmail.com>
2023-11-14 14:35:21 -0800

    Removed and ignored svelte-kit generated files

--- .svelte-kit/output/server/entries/pages/_layout.svelte.js
+++
@@ -1,8 +0,0 @@
-import { c as create_ssr_component } from "../../chunks/ssr.js";
-const app = "";
-const Layout = create_ssr_component(($$result, $$props, $$bindings, slots) => {
-  return `<div class="pt-10 pl-5">${slots.default ? slots.default({}) : ``}</div>`;
-});
-export {
-  Layout as default
-};
